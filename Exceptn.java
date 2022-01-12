4b9cf1c28849f117cb247c427d4df84ed89538e4
AmanShukla <aman.shukla@infostride.com>
2022-01-12 16:24:30 +0530

    initial change

---
+++ Exceptn.java
@@ -0,0 +1,17 @@
+public class Exceptn {
+
+                public static void main(String[] args)
+                {
+                        try {
+                                int a = 98, b = 0, c;
+                                c = a / b;
+                                System.out.println(c);
+                        }
+                        catch(Exception e)
+                        {
+                                System.out.println(e);
+                        }
+                }
+        }
+
+
